a31c1d5dca6811e923a19cf3c2845643e22725be
Lukas <lukasbaehr01@gmail.com>
2022-12-02 14:31:13 +0100

    Schule 02.12.22

---
+++ stromkosten.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Stromkosten</title>
+    </head>
+    <body>
+    <?php
+
+    $p = 2;
+    $t = 10;
+    $k = .5;
+
+    $w = $p * $t;
+    $c = $w * $k;
+
+    echo "$c EURO"
+
+    ?>
+    </body>
+</html>
