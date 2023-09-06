c21d620e26263fb43308a26c5dea0c45b2c5e976
GreenBloodDev <baehr.lukas01@gmail.com>
2023-09-06 12:19:16 +0200

    ff

---
+++ src/quatalumsatz.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quatalumsätze</title>
+    <link href="../pictures/sunlight.png" rel="icon">
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+
+$umsaetze = array(12000, 14000, 10000, 15000);
+
+for ($i = 0; $i < count($umsaetze); $i++) {
+    echo "Quatal " . ($i + 1) . ": " . $umsaetze[$i] . "€<br>";
+}
+
+?>
+
+</body>
+</html>
