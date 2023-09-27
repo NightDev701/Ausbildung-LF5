a01511978d45047e0c58b55866079c4f0ab491c4
GreenBloodDev <baehr.lukas01@gmail.com>
2023-09-27 11:34:01 +0200

    boaaaa

---
+++ src/monats_umsatz.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Monats Umsatz</title>
+</head>
+<body>
+
+<?php
+
+$monat_umsatz = [
+    "Januar" => 27,
+    "Februar" => 18,
+    "März" => 12,
+    "April" => 13,
+    "Mai" => 15,
+    "Juni" => 20,
+    "Juli" => 785,
+    "August" => 245,
+    "September" => 715,
+    "Oktober" => 295,
+    "November" => 67,
+    "Dezember" => 45
+];
+
+
+
+
+?>
+
+</body>
+</html>
